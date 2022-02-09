fb0f01934fb2691ebcf1cd54beaf7f8d9ec3790b
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-09 20:57:26 +0000

    rotate the hands.

--- JavaScript/Project_1 - Analog Clock DOM/06_02/script.js
+++ JavaScript/Project_1 - Analog Clock DOM/06_02/script.js
@@ -1,3 +1,7 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let oneHour = HOURHAND.style.transform = "rotate(30deg)"
+let oneMinute = MINUTEHAND.style.transform = "rotate(6deg)"
+let oneSecond = SECONDHAND.style.transform = "rotate(1.2deg)"
