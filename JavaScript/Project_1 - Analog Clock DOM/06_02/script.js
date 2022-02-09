84c6982247fd903de655f8ad00804decb7720a50
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-09 21:07:05 +0000

    rotate hands by the date time

--- JavaScript/Project_1 - Analog Clock DOM/06_02/script.js
+++ JavaScript/Project_1 - Analog Clock DOM/06_02/script.js
@@ -1,7 +1,15 @@
-const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute");
-const SECONDHAND = document.querySelector("#second");
+const HOURHAND = document.querySelector('#hour');
+const MINUTEHAND = document.querySelector('#minute');
+const SECONDHAND = document.querySelector('#second');
 
-let oneHour = HOURHAND.style.transform = "rotate(30deg)"
-let oneMinute = MINUTEHAND.style.transform = "rotate(6deg)"
-let oneSecond = SECONDHAND.style.transform = "rotate(1.2deg)"
+const date = new Date();
+const [hour, minutes, seconds] = [
+  date.getHours(),
+  date.getMinutes(),
+  date.getSeconds(),
+];
+
+let oneHour = (HOURHAND.style.transform = `rotate(${30 * (hour - 12)}deg)`);
+let oneMinute = (MINUTEHAND.style.transform = `rotate(${6 * minutes}deg)`);
+let oneSecond = (SECONDHAND.style.transform = 
+    `rotate(${1.2*seconds}deg)`);
