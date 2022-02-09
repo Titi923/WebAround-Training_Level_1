df0a712f0b71e1a8c5629091210f958f29e9ff89
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-09 22:30:29 +0000

    fixing return to zero bug

--- JavaScript/Project_1 - Analog Clock DOM/06_02/script.js
+++ JavaScript/Project_1 - Analog Clock DOM/06_02/script.js
@@ -1,18 +1,27 @@
-const clock = function () {
-  const HOURHAND = document.querySelector('#hour');
-  const MINUTEHAND = document.querySelector('#minute');
-  const SECONDHAND = document.querySelector('#second');
+const HOURHAND = document.querySelector('#hour');
+const MINUTEHAND = document.querySelector('#minute');
+const SECONDHAND = document.querySelector('#second');
 
-  const date = new Date();
-  const [hour, minutes, seconds] = [
-    date.getHours(),
-    date.getMinutes(),
-    date.getSeconds(),
-  ];
+var date = new Date();
+let [hour, minutes, seconds] = [
+  date.getHours(),
+  date.getMinutes(),
+  date.getSeconds(),
+];
 
-  let oneHour = (HOURHAND.style.transform = `rotate(${30 * (hour - 12)}deg)`);
-  let oneMinute = (MINUTEHAND.style.transform = `rotate(${6 * minutes}deg)`);
-  let oneSecond = (SECONDHAND.style.transform = `rotate(${6 * seconds}deg)`);
+let oneHour = (hour*360/12)+(minutes*(360/60)/12);
+let oneMinute = (minutes*360/60)+(seconds*(360/60)/60);
+let oneSecond = seconds*360/60;
+
+clock = function () {
+  oneHour = oneHour+(3/360);
+  oneMinute = oneMinute+(6/60);
+  oneSecond = oneSecond+6;
+
+  HOURHAND.style.transform = `rotate(${oneHour}deg)`;
+  MINUTEHAND.style.transform = `rotate(${oneMinute}deg)`;
+  SECONDHAND.style.transform = `rotate(${oneSecond}deg)`;
+  console.log(oneSecond);
 };
 
 let intervalID = setInterval(clock, 1000);
