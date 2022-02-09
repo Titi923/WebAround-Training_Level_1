b5cf891b27445acbfdb18d167cbe7e5e2b317cbd
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-09 22:13:38 +0000

    creating the lcok

--- JavaScript/Project_1 - Analog Clock DOM/06_02/script.js
+++ JavaScript/Project_1 - Analog Clock DOM/06_02/script.js
@@ -1,15 +1,18 @@
-const HOURHAND = document.querySelector('#hour');
-const MINUTEHAND = document.querySelector('#minute');
-const SECONDHAND = document.querySelector('#second');
+const clock = function () {
+  const HOURHAND = document.querySelector('#hour');
+  const MINUTEHAND = document.querySelector('#minute');
+  const SECONDHAND = document.querySelector('#second');
 
-const date = new Date();
-const [hour, minutes, seconds] = [
-  date.getHours(),
-  date.getMinutes(),
-  date.getSeconds(),
-];
+  const date = new Date();
+  const [hour, minutes, seconds] = [
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+  ];
 
-let oneHour = (HOURHAND.style.transform = `rotate(${30 * (hour - 12)}deg)`);
-let oneMinute = (MINUTEHAND.style.transform = `rotate(${6 * minutes}deg)`);
-let oneSecond = (SECONDHAND.style.transform = 
-    `rotate(${1.2*seconds}deg)`);
+  let oneHour = (HOURHAND.style.transform = `rotate(${30 * (hour - 12)}deg)`);
+  let oneMinute = (MINUTEHAND.style.transform = `rotate(${6 * minutes}deg)`);
+  let oneSecond = (SECONDHAND.style.transform = `rotate(${6 * seconds}deg)`);
+};
+
+let intervalID = setInterval(clock, 1000);
