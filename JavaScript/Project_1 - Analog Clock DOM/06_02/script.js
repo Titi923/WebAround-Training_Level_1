f04bae2160ea5df3293c1b07529699fa2fdc6314
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-09 22:50:41 +0000

    finish clock

--- JavaScript/Project_1 - Analog Clock DOM/06_02/script.js
+++ JavaScript/Project_1 - Analog Clock DOM/06_02/script.js
@@ -9,19 +9,18 @@ let [hour, minutes, seconds] = [
   date.getSeconds(),
 ];
 
-let oneHour = (hour*360/12)+(minutes*(360/60)/12);
-let oneMinute = (minutes*360/60)+(seconds*(360/60)/60);
-let oneSecond = seconds*360/60;
+let oneHour = (hour * 360) / 12 + (minutes * (360 / 60)) / 12;
+let oneMinute = (minutes * 360) / 60 + (seconds * (360 / 60)) / 60;
+let oneSecond = (seconds * 360) / 60;
 
 clock = function () {
-  oneHour = oneHour+(3/360);
-  oneMinute = oneMinute+(6/60);
-  oneSecond = oneSecond+6;
+  oneHour = oneHour + 3 / 360;
+  oneMinute = oneMinute + 6 / 60;
+  oneSecond = oneSecond + 6;
 
   HOURHAND.style.transform = `rotate(${oneHour}deg)`;
   MINUTEHAND.style.transform = `rotate(${oneMinute}deg)`;
   SECONDHAND.style.transform = `rotate(${oneSecond}deg)`;
-  console.log(oneSecond);
 };
 
 let intervalID = setInterval(clock, 1000);
