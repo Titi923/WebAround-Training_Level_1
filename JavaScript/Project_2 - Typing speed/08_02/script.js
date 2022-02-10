71044c544500a3b240fe86ae84825de8bcf3a912
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-10 18:47:51 +0000

    getting the dom

--- JavaScript/Project_2 - Typing speed/08_02/script.js
+++ JavaScript/Project_2 - Typing speed/08_02/script.js
@@ -11,13 +11,23 @@ const theTimer = document.querySelector(".timer");
 
 
 // Match the text entered with the provided text on the page:
-
+const spellCheck = function() {
+    let textEntered = testArea.value;
+    console.log(textEntered);
+}
 
 // Start the timer:
-
+const start = function() {
+    let textEnteredLength = testArea.value.length;
+    console.log(textEnteredLength);
+}
 
 // Reset everything:
-
+const reset = function() {
+    
+}
 
 // Event listeners for keyboard input and the reset button:
 testArea.addEventListener('keypress', start)
+testArea.addEventListener('keyup', spellCheck)
+resetButton.addEventListener("click", reset)
