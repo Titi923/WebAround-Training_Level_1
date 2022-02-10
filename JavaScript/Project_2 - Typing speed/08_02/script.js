2c3a56a7740dd9b1a49b33f96b541f01e587b3b6
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-10 18:51:34 +0000

    create the timer

--- JavaScript/Project_2 - Typing speed/08_02/script.js
+++ JavaScript/Project_2 - Typing speed/08_02/script.js
@@ -8,7 +8,15 @@ const theTimer = document.querySelector(".timer");
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 // Run a standard minute/second/hundredths timer:
+function runTimer() {
+    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
 
+    timer[0] = Math.floor((timer[3]/100)/60);
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
 
 // Match the text entered with the provided text on the page:
 const spellCheck = function() {
@@ -24,7 +32,7 @@ const start = function() {
 
 // Reset everything:
 const reset = function() {
-    
+
 }
 
 // Event listeners for keyboard input and the reset button:
