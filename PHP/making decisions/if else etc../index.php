a3f6c74b04748fc7cdda553a0668820b77601ee3
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-11 18:07:24 +0000

    learning logical operators

---
+++ PHP/making decisions/if else etc../index.php
@@ -0,0 +1,11 @@
+<?php
+    $rest_name = "Pizza Riba";
+    $num = 18;
+    if ($num <= 18) {
+        echo "minor is true"
+    ;}
+    elseif ($num >= 18 and $num <=50) {
+        echo 'adult'
+    ;} else echo 'idk what you are'
+
+?>
