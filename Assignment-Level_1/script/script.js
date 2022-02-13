d94ea776b92e8e5dbe849ae8004d4f356f9cb2c0
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-13 19:35:57 +0000

    adding map.

--- Assignment-Level_1/script/script.js
+++ Assignment-Level_1/script/script.js
@@ -1,2 +1,27 @@
-'use strict';
-console.log('works')
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 44.411161, lng: 26.079081 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 9,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+// const myLatLng = { lat: -25.363, lng: 131.044 };
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: myLatLng,
+//   });
+
+//   new google.maps.Marker({
+//     position: myLatLng,
+//     map,
+//     title: "Hello World!",
+//   });
