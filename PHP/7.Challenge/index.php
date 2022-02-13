34d895a4f725348e8d4a2a7a9b6993f635420cfd
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-13 08:59:29 +0000

    7. adding a possible case for if

--- PHP/7.Challenge/index.php
+++ PHP/7.Challenge/index.php
@@ -1,25 +1,31 @@
 <?php
-    function copyright() {
-        $release_year = 2019;
-        $current_year = date('Y');
+function copyright()
+{
+    $release_year = 2019;
+    $current_year = date('Y');
+    if ($release_year == $current_year) {
+        $final = "&copy $current_year";
+    } else {
         $final = "&copy $release_year - $current_year";
-        return $final;
     }
+    return $final;
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Challenge 7-copyright</title>
 </head>
+
 <body>
     <h1>
-        <?php
-    echo copyright();
-    ?>
+        <?= copyright(); ?>
     </h1>
 </body>
+
 </html>
