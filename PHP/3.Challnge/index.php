8fa4446d5fecc34990ce76cd04f88210453191b0
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-13 08:48:40 +0000

    4. php-fix spacing and code.

--- PHP/3.Challnge/index.php
+++ PHP/3.Challnge/index.php
@@ -1,10 +1,11 @@
 <?php
-    $rest_name = "Pizza Riba";
-    $arr = ['Pizza', 'Burger', 'Pasta'];
+$rest_name = "Pizza Riba";
+$arr = ['Pizza', 'Burger', 'Pasta'];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,8 +13,10 @@
     <title>Challenge_1 Emmbed php in html</title>
     <link rel="stylesheet" href="/style.css">
 </head>
+
 <body>
-    <h1><?= $rest_name?></h1>
-    <h2>Today's speciality is <?= $arr[0]?></h2>
+    <h1><?= $rest_name ?></h1>
+    <h2>Today's speciality is <?= $arr[0] ?></h2>
 </body>
+
 </html>
--- PHP/4.Challenge/index.php
+++ PHP/4.Challenge/index.php
@@ -1,39 +1,32 @@
-<?php
-    $islogged = false;
-?>
+<?php $islogged = false; ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Different php</title>
 </head>
+
 <body>
-    <?php
-        if ($islogged || !$islogged) {
-    ?>
     <h1 class="both">This content is for both</h1>
     <p>members and non-member</p>
     <?php
-    }
+    if ($islogged) :
     ?>
-    
+        <h1 class="Member">You are a member</h1>
     <?php
-    if ($islogged) {        
-        ?>
-    <h1 class="Member">You are a member</h1>
-    <?php
-}   
+    endif;
     ?>
-    
     <?php
-    if (!$islogged) {        
-        ?>
-    <h1 class="Non-Member">You are not a member</h1>
+    if (!$islogged) :
+    ?>
+        <h1 class="Non-Member">You are not a member</h1>
     <?php
-}   
+    endif;
     ?>
 </body>
+
 </html>
