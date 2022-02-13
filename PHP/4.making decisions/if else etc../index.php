a96ca0f4a1bb4da807f586d4df3555a361f18eae
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-13 08:55:52 +0000

    4. making decision fixing spaces.

--- PHP/4.making decisions/if else etc../index.php
+++ PHP/4.making decisions/if else etc../index.php
@@ -1,11 +1,8 @@
 <?php
-    $rest_name = "Pizza Riba";
-    $num = 18;
-    if ($num <= 18) {
-        echo "minor is true"
-    ;}
-    elseif ($num >= 18 and $num <=50) {
-        echo 'adult'
-    ;} else echo 'idk what you are'
-
-?>
+$rest_name = "Pizza Riba";
+$num = 19;
+if ($num <= 18) {
+    echo "minor is true";
+} elseif ($num >= 18 and $num <= 50) {
+    echo 'adult';
+} else echo "I don't care about your age you can eat at $rest_name !";
