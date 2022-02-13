8a8eb6ea781a81899f7d672751d870230e01d9ef
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-13 09:23:48 +0000

    fixing indentation php 7

--- PHP/6.Challenge/index.php
+++ PHP/6.Challenge/index.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,31 +8,33 @@
     <link rel="stylesheet" href="/Challnge_1/6.Challenge/style.css">
     <title>Multiplication table</title>
 </head>
+
 <body>
     <table>
-    <!-- First row of the table! -->
-    <?php
-    echo '<tr>';
-    echo '<th> </th>';
-    for ($col = 1; $col <= 10; $col++) {
-        echo "<th>$col</th>";
-    }
-    echo '</tr>';
-    // Create rest of rows
-    for($row = 1, $col = 1; $row <= 10; $row++) {
+        <!-- First row of the table! -->
+        <?php
         echo '<tr>';
-        // First cell
-        if ($col == 1) {
-            echo "<th>$row</th>";
-        }
-        while ($col <= 10) {
-            echo '<td>',$row * $col++,'</td>';
+        echo '<th> </th>';
+        for ($col = 1; $col <= 10; $col++) {
+            echo "<th>$col</th>";
         }
         echo '</tr>';
-        // Reset col at the end of each row
-        $col = 1;
-    }
-?>
+        // Create rest of rows
+        for ($row = 1, $col = 1; $row <= 10; $row++) {
+            echo '<tr>';
+            // First cell
+            if ($col == 1) {
+                echo "<th>$row</th>";
+            }
+            while ($col <= 10) {
+                echo '<td>', $row * $col++, '</td>';
+            }
+            echo '</tr>';
+            // Reset col at the end of each row
+            $col = 1;
+        }
+        ?>
     </table>
 </body>
+
 </html>
