ddce66471540a7ac489ed79b1b451201eb3cba91
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-14 01:25:42 +0000

    format text

---
+++ Assignment-Level_1/town.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <!-- Add facebook snippet for video -->
+    <title>De la obiectiv la rezultat</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&family=Open+Sans:wght@300;400&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/54e170f569.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav>
+        <ul class="navbar container">
+            <li><img id="andy" src="./images/Andy_Logo.png" alt="" /></li>
+            <li><img id="booe" src="./images/Booe_Camp_Logo.png" alt="" /></li>
+        </ul>
+    </nav>
+    <!-- header -->
+    <header class="container">
+        <div>
+            <img class="img-header" src="./images/HeaderPhoto.png" alt="" />
+        </div>
+    </header>
+    <!-- Footer -->
+    <footer class="footer">
+        <p class="text-center copy">&copy; Copyright 2014-2022</p>
+        <div>
+            <ul class="container footer-links">
+                <li><i class="fa-solid fa-file-lines"></i> <a href="">Termeni si conditii</a></li>
+                <li><i class="fa-solid fa-bars"></i> <a href="">Conditii generale de paricipare la programe</a></li>
+                <li><i class="fa-solid fa-lock"></i> <a href="">Protectia datelor cu caracter personal</a></li>
+            </ul>
+        </div>
+    </footer>
+    <script src="./script/script.js"></script>
+</body>
+
+</html>
