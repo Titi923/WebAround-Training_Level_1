27680a1e5ee09888a503e36cf12fe982d123b6fb
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-14 17:56:40 +0000

    change alittle style.

--- Assignment-Level_1/script/script.js
+++ Assignment-Level_1/script/script.js
@@ -3,7 +3,7 @@ function initMap() {
   // The location of Uluru
   const uluru = { lat: 44.411161, lng: 26.079081 };
   // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
+  const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 9,
     center: uluru,
   });
@@ -14,25 +14,34 @@ function initMap() {
   });
 }
 
+// Countown Timer
 function startTimer(duration, display) {
-  let timer = duration, minutes, seconds;
+  let timer = duration,
+    minutes,
+    seconds;
   setInterval(function () {
-      minutes = parseInt(timer / 60, 10);
-      seconds = parseInt(timer % 60, 10);
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
 
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
 
-      display.textContent = minutes + ":" + seconds;
+    display.textContent = minutes + ':' + seconds;
 
-      if (--timer < 0) {
-          display = "00:00:00"
-      }
+    if (--timer < 0) {
+      display = '00:00:00';
+    }
   }, 1000);
 }
-
 window.onload = function () {
   var fifteenMinutes = 60 * 15,
-      display = document.querySelector('#time');
+    display = document.querySelector('#time');
   startTimer(fifteenMinutes, display);
 };
+
+
+// Price on php page
+
+let price = document.getElementById('thePrice').innerHTML
+price = 1;
+console.log(price);
